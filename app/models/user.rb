b98b0f6cc6cd9f685012a9937c5ee48f90fe2346
garlandpool <frederic.pool@threerivers.k12.or.us>
2015-11-19 13:12:26 -0800

    Reviews scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :movies
+	has_many :reviews, dependent: :destroy # This will delete reviews when a user is destroyed
+	
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable,
